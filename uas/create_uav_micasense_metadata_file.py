182d59874e5505382693ed0d866ac1db6d12ee4b
mlucas <mark.lucas.cork@gmail.com>
2017-07-28 15:24:56 +0100

    Commit to GIT repository

--- uas/create_uav_micasense_metadata_file.py
+++ uas/create_uav_micasense_metadata_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
 #
 # Program: create_uas_metadata_file_micasense
 #
@@ -17,7 +21,6 @@
 
 __author__ = 'mlucas'
 
-
 import csv
 import math
 import time
@@ -26,6 +29,13 @@ import os
 import argparse
 import imagepreprocess
 from imagepreprocess import *
+from shapely import wkt
+from shapely.wkt import dumps
+from shapely.geometry import Point
+from decimal import *
+
+
+
 
 print imagepreprocess.__name__
 print sys.path
@@ -54,7 +64,10 @@ uasmetfile = args.out
 
 record_id = None
 notes = ''
-metadatalist = []
+metadataRecord=[]
+metadataList = []
+getcontext().prec = 8
+getcontext()
 
 imagefiles = get_image_file_list(uasPath, imageType)
 if len(imagefiles)==0:
@@ -66,16 +79,16 @@ if len(imagefiles)==0:
 
 firstImage=uasPath + imagefiles[0]
 with open(firstImage,'rb') as image:
-    cam_position_x, cam_position_y, cam_position_z, cam_latitude, cam_longitude, cam_sample_date_utc, \
-    cam_sample_time_utc, cam_lat_zone, cam_long_zone, cam_altitude_ref, cam_serial_no = get_image_exif_data(image)
+     position_x, position_y, altitudeFeet, latitude, longitude, dateUTC, \
+     timeUTC, lat_zone, long_zone, altitudeRef, cam_serial_no = get_image_exif_data(image)
 image.close()
-y = cam_sample_date_utc[0:4]
-m = cam_sample_date_utc[5:7]
-d = cam_sample_date_utc[8:10]
+y = dateUTC[0:4]
+m = dateUTC[5:7]
+d = dateUTC[8:10]
 dateString = y + m + d
-h = cam_sample_time_utc[0:2]
-mm = cam_sample_time_utc[3:5]
-s = cam_sample_time_utc[6:8]
+h = timeUTC[0:2]
+mm = timeUTC[3:5]
+s = timeUTC[6:8]
 timeString = h + mm + s
 flightId = 'uas_' + dateString + '_' + timeString
 
@@ -88,70 +101,61 @@ for f in imagefiles:
     #Get Image File EXIF metadata
     #
     with open(filename,'rb') as image:
-        print "Processing ",filename
-        cam_position_x, cam_position_y,cam_position_z, cam_latitude, cam_longitude, cam_sample_date_utc, \
-        cam_sample_time_utc, cam_lat_zone,cam_long_zone,cam_altitude_ref,cam_serial_no=get_image_exif_data(image)
+         #print "Processing ",filename
+         position_x, position_y,altitudeFeet, latitude, longitude, dateUTC, \
+         timeUTC, lat_zone,long_zone,altitudeRef,cam_serial_no=get_image_exif_data(image)
     image.close()
 
     # FlightID calculation is incorrect - Should only use the first image date and time
 
-    y=cam_sample_date_utc[0:4]
-    m=cam_sample_date_utc[5:7]
-    d=cam_sample_date_utc[8:10]
+    y=dateUTC[0:4]
+    m=dateUTC[5:7]
+    d=dateUTC[8:10]
     dateString = y+m+d
-    h= cam_sample_time_utc[0:2]
-    mm= cam_sample_time_utc[3:5]
-    s= cam_sample_time_utc[6:8]
+    h= timeUTC[0:2]
+    mm= timeUTC[3:5]
+    s= timeUTC[6:8]
     timeString=h+mm+s
-    sensor_id = cam_serial_no
+    sensorId = cam_serial_no
+    getcontext().prec=7
+    lonDec=Decimal(str(longitude))
+    latDec=Decimal(str(latitude))
+    position = dumps(Point(lonDec, latDec),rounding_precision=7)
+    #position = Point(lonDec, latDec)
+    positionRef = 1
+    notes=None
     # Rename image files
-    imagename='CAM_' + sensor_id + '_' + dateString + '_' + timeString+ '_' +  imagefilename
+    imageFileName='CAM_' + sensorId + '_' + dateString + '_' + timeString+ '_' +  imagefilename
     oldImageFilePath= uasPath + imagefilename
-    newImageFilePath= uasPath + imagename
+    newImageFilePath= uasPath + imageFileName
     os.rename (oldImageFilePath,newImageFilePath)
     # Populate the metadata data structure for the renamed image
-    metadata_record = init_metadata_record(flightId, sensor_id)
-    metadata_record[24] = calculate_checksum(newImageFilePath)
-    metadata_record[0]=record_id
-    metadata_record[1]=imagename
-    metadata_record[2]=flightId
-    metadata_record[3]=sensor_id
-    metadata_record[14]=cam_position_x
-    metadata_record[15]=cam_position_y
-    metadata_record[16]=cam_position_z
-    metadata_record[17]=cam_latitude
-    metadata_record[18]=cam_longitude
-    metadata_record[19]=cam_sample_date_utc
-    metadata_record[20]=cam_sample_time_utc
-    metadata_record[21]=cam_lat_zone
-    metadata_record[22]=cam_long_zone
-    metadata_record[23]=cam_altitude_ref
-    time.sleep(0.1)
-    metadatalist.append(metadata_record)
+    #metadata_record = init_metadata_record(flightId, sensorId)
+    metadataRecord=[]
+    altitude=float(altitudeFeet) * 0.3048
+    md5sum = calculate_checksum(newImageFilePath)
+    #metadataRecord=[imageFileName,flightId,sensorId,dateUTC,timeUTC,position.wkt,altitude,altitudeRef,md5sum,positionRef,notes]
+    metadataRecord = [imageFileName, flightId, sensorId, dateUTC, timeUTC, position, altitude, altitudeRef, md5sum,positionRef, notes]
+    metadataList.append(metadataRecord)
+    metadataList.append(metadataRecord)
     camIndex += 1
 
 #
 # Write out the metadata file
 #
 with open(uasmetfile, 'wb') as csvfile:
-    header = csv.writer(csvfile)
-    header.writerow(
-        ['record_id', 'image_file_name','flight_id', 'sensor_id', 'uas_position_x',
-         'uas_position_y', 'uas_position_z', 'uas_latitude','uas_longitude','uas_sampling_date_utc','uas_sampling_time_utc',
-         'uas_lat_zone', 'uas_long_zone','uas_altitude_reference','cam_position_x','cam_position_y', 'cam_position_z',
-         'cam_latitude','cam_longitude','cam_sampling_date_utc','cam_sampling_time_utc','cam_lat_zone', 'cam_long_zone',
-         'cam_altitude_reference', 'md5sum', 'notes'])
+    header = csv.writer(csvfile,lineterminator = '\n')
+    header.writerow(['image_file_name', 'flight_id', 'sensor_id', 'date_utc', 'time_utc', 'position','altitude',
+                     'altitude_ref', 'md5sum', 'position_source', 'notes'])
 csvfile.close()
 
 with open(uasmetfile, 'ab') as csvfile:
     print 'Generating metadata file', uasmetfile
-    for lineitem in metadatalist:
-        fileline = csv.writer(csvfile)
+    for lineitem in metadataList:
+        fileline = csv.writer(csvfile,quoting=csv.QUOTE_ALL,lineterminator = ',\n')
         fileline.writerow(
             [lineitem[0], lineitem[1], lineitem[2],lineitem[3], lineitem[4], lineitem[5], lineitem[6], lineitem[7],
-             lineitem[8], lineitem[9], lineitem[10], lineitem[11], lineitem[12], lineitem[13], lineitem[14],
-             lineitem[15],lineitem[16],lineitem[17],lineitem[18],lineitem[19],lineitem[20],lineitem[21],lineitem[22],
-             lineitem[23],lineitem[24]])
+             lineitem[8], lineitem[9], lineitem[10]])
 
 csvfile.close()
 
